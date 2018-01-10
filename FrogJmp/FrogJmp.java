bba749526e01c72ca58f2b65737747d18d787749
Pawel Hericht <p.hericht@gmail.com>
2018-01-10 18:48:57 +0100

    task 3.1

--- FrogJmp/FrogJmp.java
+++ FrogJmp/FrogJmp.java
@@ -1,10 +1,10 @@
 package FrogJmp;
 
 public class FrogJmp {
+	
 	public int solution(int X, int Y, int D) {
 
 		int distance = Y - X;
-
 		return distance % D == 0 ? distance / D : distance / D + 1;
 	}
 }
