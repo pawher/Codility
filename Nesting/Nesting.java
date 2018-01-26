2074af2cb41e88d63b9c530e0aa489d1a6a07c9c
Pawel Hericht <p.hericht@gmail.com>
2018-01-26 15:58:30 +0100

    task 7.4

---
+++ Nesting/Nesting.java
@@ -0,0 +1,37 @@
+package Nesting;
+
+import java.util.Stack;
+
+public class Nesting {
+
+	public static void main(String[] args) {
+		System.out.println(solution("(())"));
+	}
+
+	public static int solution(String S) {
+
+		if (S.isEmpty()) {
+			return 1;
+		}
+
+		if (S.length() % 2 == 1 || S.isEmpty() || S.charAt(0) == ')' || S.charAt(S.length() - 1) == '(') {
+			return 0;
+		}
+
+		Stack<Character> stack = new Stack<>();
+		for (char k : S.toCharArray()) {
+			if (k == '(') {
+				stack.push(k);
+			} else {
+				if (stack.isEmpty()) {
+					return 0;
+				} else {
+					stack.pop();
+				}
+			}
+		}
+
+		return stack.isEmpty() ? 1 : 0;
+
+	}
+}
