3ce5e4cd1d47860a74ed75abe1dd3b7b3cf23534
Pawel Hericht <p.hericht@gmail.com>
2018-01-30 15:44:13 +0100

    task 15.1

--- AbsDistinct/AbsDictinct.java
+++ AbsDistinct/AbsDictinct.java
@@ -5,22 +5,22 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class AbsDictinct {
-	
+
 	public int solution(int[] A) {
-        Set<Integer> set = new HashSet<>();
-        
-		for(int i=0; i<A.length; i++) {
-			if(A[i] < 0) {
+		Set<Integer> set = new HashSet<>();
+
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] < 0) {
 				set.add(Math.abs(A[i]));
 			} else {
 				set.add(A[i]);
 			}
 		}
-		
-		return set.size();
-		
-		//Arrays.stream(A).map(x-> Math.abs(x)).distinct().count();
 
-    }
+		return set.size();
+
+		// Arrays.stream(A).map(x-> Math.abs(x)).distinct().count();
+
+	}
 
 }
